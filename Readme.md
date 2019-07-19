4b125273949fe6e12c96f7d4a4da1a04412dbbe1
Matthew Edwards <mje-nz@users.noreply.github.com>
2019-07-19 16:37:25 +1200

    Tidy up

--- Readme.md
+++ Readme.md
@@ -22,6 +22,14 @@ you can insert it into a LaTeX document like this:
 
 By default, the figure size defaults to `\textwidth` square and the font and font size match the rest of the document.
 
+Wishlist:
+
+* Better Readme
+* Tests
+* CI
+* Inline figures
+* Tables
+
 Tested with Python 3.7, matplotlib 3.0.3, PythonTeX 0.16, and latexmk 4.63b.
 Note that PythonTeX 0.16 is quite outdated; `example.tex` includes a patch for one issue which is fixed upstream but not yet released.
 Inspired by [TheChymera/RepSeP](https://github.com/TheChymera/RepSeP) (but much simpler) and [this blog post by Bennett Kanuka](http://bkanuka.com/posts/native-latex-plots/) (but more automated).
--- python_figures.py
+++ python_figures.py
@@ -9,6 +9,7 @@ import os
 import os.path
 import re
 import textwrap
+from typing import Callable, Optional
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -19,19 +20,19 @@ GOLDEN = (1.0 + math.sqrt(5.0))/2.0
 
 # Placeholders for setup()
 pytex = None
-FONT_SIZE = None
-TEXT_WIDTH = None
-SCRIPTS_DIR = None
-DATA_DIR = None
-FIGURES_DIR = None
+FONT_SIZE = None  # type: Optional[float]
+TEXT_WIDTH = None  # type: Optional[float]
+SCRIPTS_DIR = None  # type: Optional[str]
+DATA_DIR = None  # type: Optional[str]
+FIGURES_DIR = None  # type: Optional[str]
 
 
 def _setup_paths(scripts_dir=None, data_dir=None):
     """Parse latexmkrc for path definitions, optionally overriding with arguments.
 
     Args:
-        scripts_dir (str): Directory containing figure scripts (overrides latexmkrc)
-        data_dir (str): Directory containing figure data files (overrides latexmkrc)
+        scripts_dir (str): Directory containing figure scripts (overrides latexmkrc).
+        data_dir (str): Directory containing figure data files (overrides latexmkrc).
     """
     global SCRIPTS_DIR, DATA_DIR, FIGURES_DIR
 
@@ -39,7 +40,7 @@ def _setup_paths(scripts_dir=None, data_dir=None):
     if os.path.exists('latexmkrc'):
         regex = re.compile(r"\$pythontex_(\w*?)_dir = [\'\"](\w*?)[\'\"];$")
         with open('latexmkrc') as latexmkrc:
-            for line in latexmkrc.readlines():
+            for line in latexmkrc:
                 line = line.strip()
                 match = regex.search(line)
                 if match:
@@ -116,17 +117,19 @@ def _setup_matplotlib(font_size=None):
 
 
 def setup(pytex_, *, scripts_dir=None, data_dir=None):
-    """Set up module (call before importing pyplot!)
+    """Configure matplotlib, resolve paths to figure scripts and data, and save pytex
+    context.
 
-    TODO
+    Call this at the start of the PythonTeX custom code, **before** importing
+    matplotlib.
 
-    By default the directories configured in latexmkrc are used, but you can override
-    them here.
+    By default the script/data directories from latexmkrc are used, but you can set them
+    here instead if necessary.
 
     Args:
-        pytex_ (module): The global PythonTeXUtils instance
-        scripts_dir (str): Directory containing figure scripts (overrides latexmkrc)
-        data_dir (str): Directory containing figure data files (overrides latexmkrc)
+        pytex_ (module): The global PythonTeXUtils instance.
+        scripts_dir (str): Directory containing figure scripts (overrides latexmkrc).
+        data_dir (str): Directory containing figure data files (overrides latexmkrc).
     """
     global pytex, FONT_SIZE, TEXT_WIDTH
 
@@ -145,19 +148,22 @@ def setup(pytex_, *, scripts_dir=None, data_dir=None):
     _setup_matplotlib()
 
 
-# https://stackoverflow.com/a/41658338
 def _load_script(script_name):
-    """Load the main() function from the given script.
+    """Load the main() function from the given script, such that it will run in the
+    PythonTeX session's namespace.
 
     Args:
         script_name (str): The filename of the script, either as an absolute path or
             relative to the scripts directory.
+
+    Returns:
+        Callable[..., str]: The main() function.
     """
     script_path = os.path.join(SCRIPTS_DIR, script_name)
 
-    # Copy the globals from the main pytex namespace (so imports from the setup block
-    # are present), but set __file__ and __name__ such that it looks like it's being
-    # imported normally.  Note that this will totally break if this function isn't
+    # Copy the globals from the PythonTeX session's namespace (so imports from the setup
+    # block are present), but set __file__ and __name__ such that it looks like it's
+    # being imported normally.  Note that this will totally break if this function isn't
     # exactly two calls down from a PythonTeX environment.
     assert inspect.stack()[2].filename.startswith('pythontex-files-')
     globals_ = inspect.stack()[2].frame.f_globals.copy()
@@ -166,64 +172,102 @@ def _load_script(script_name):
         "__name__": script_name,
     })
 
+    # https://stackoverflow.com/a/41658338
     with pytex.open(script_path, 'rb') as file:
         exec(compile(file.read(), filename=script_path, mode='exec'), globals_)
     return globals_['main']
 
 
-def _render_figure(figure_func, width, aspect, format_='pgf'):
-    """Actually render the figure, without any PythonTeX-specific tricks."""
-    figsize = (width, width/aspect)
-    plt.figure(figsize=figsize)
+def _draw_figure(figure_func, width, aspect, format_='pgf'):
+    """Set up a matplotlib figure, call a function to draw in it, then save it in the
+    given format and return the filename.
 
+    Args:
+        figure_func (Callable[[], str]): A function which takes no arguments, draws a
+            figure, and returns a unique name for the figure.
+        width (float): The figure width in inches.
+        aspect (float): The figure aspect ratio.
+        format_ (str): The file format in which to save the figure ('pdf' or 'pgf').
+
+    Returns:
+        str: The saved figure's filename.
+    """
+    figure_size = (width, width/aspect)
+    plt.figure(figsize=figure_size)
+
+    # TODO: Stub out plt.figure
     # Run figure function, then reset mpl.rcParams
     with mpl.rc_context():
         name = figure_func()
     # Generate name for figure
     # TODO: Is this a sensible way to define the filename?
     assert name is not None
-    name += '-%.2fx%.2f' % figsize
+    name += '-%.2fx%.2f' % figure_size
 
     assert os.path.isdir(FIGURES_DIR), "Figures dir does not exist"
     figure_filename = os.path.join(FIGURES_DIR, name + '.' + format_)
     plt.savefig(figure_filename, bbox_inches='tight')
+    # TODO: Close figures?
     return figure_filename
 
 
 def figure(script_name, *args, width=TEXT_WIDTH, aspect=SQUARE, **kwargs):
     r"""Insert a figure from a Python script.
 
-    The script will be executed as if imported from inside the scripts directory, but
-    with numpy etc already in the global namespace.  Before main is executed, a figure
-    of appropriate size is created.  After main returns, the figure will be saved and
-    inserted into the document.
+    The script should contain a function called `main`, which draws onto a
+    pre-configured matplotlib figure and returns a unique name (without extension) for
+    the figure.  The figure will then be saved and included as a PGF in the document.
+    Any setup done in the document's pythontexcustomcode environment will be available.
 
-    Leftover positional and keyword arguments will be passed to main.  Main should
-    return a unique filename (without extension).  Any files accessed in main should
-    either be opened using pytex.open or passed to pytex.add_dependencies or
-    pytex.add_created.
+    `main` will be called with any leftover arguments to this function.
+
+    Any files read in `main` should either be opened using `pytex.open` or passed to
+    `pytex.add_dependencies` (so that pythontex re-runs the script when they change),
+    and should be in `DATA_DIR` (so that latexmk triggers a build when they change).
+
+    Any files written in `main` should either be opened using `pytex.open` or passed
+    to `pytex.add_created` (so that pythontex deletes the old file when it is renamed),
+    and should go in `FIGURES_DIR`, (so that latexmk removes them on clean).
 
     Args:
         script_name (str): The filename of the script, either as an absolute path or
             relative to the scripts directory.
         width (float): The figure width in inches (defaults to \textwidth).  For a
-            fraction of \textwidth, use e.g. 0.5*textwidth()
+            fraction of \textwidth, use the TEXT_WIDTH constant (e.g., 0.5*TEXT_WIDTH).
         aspect (float): The figure aspect ratio (SQUARE, GOLDEN, or a number).
+
+    Returns:
+        str: The LaTeX markup which includes the figure in the document.
     """
     if width is None:
-        # TEXT_WIDTH is calculated  when setup runs, so it doesn't actually work as a
+        # TEXT_WIDTH is calculated when setup runs, so it doesn't actually work as a
         # default argument
         width = TEXT_WIDTH
 
     main = _load_script(script_name)
-    figure_filename = _render_figure(lambda: main(*args, **kwargs), width, aspect)
+    figure_filename = _draw_figure(lambda: main(*args, **kwargs), width, aspect)
 
     pytex.add_created(figure_filename)
     return r'\input{%s}' % figure_filename
 
 
-def _run_setup_code(pytxcode_file, globals_):
-    """Run the setup code in the given namespace to get the right imports."""
+def _run_setup_code(document_name, globals_):
+    """Run the PythonTeX setup code from the given document in the given namespace.
+
+    A .pytxcode file generated by PythonTeX is parsed to find the first custom code
+    block, then the code is extracted and executed in the given namespace (except for
+    calls to `setup`).
+
+    Args:
+        document_name (str): The name (excluding extension) of a tex document processed
+            by PythonTeX which contains a pythontexcustomcode environment.
+        globals_ (dict): The globals() dictionary from the namespace in which to run the
+            setup code.
+    """
+    document_name = os.path.basename(document_name)
+    pytxcode_file = document_name + '.pytxcode'
+    assert os.path.exists(pytxcode_file)
+
     with open(pytxcode_file, 'r') as pytxcode:
         lines = pytxcode.readlines()
     # Find the pythontexcustomcode section
@@ -241,21 +285,36 @@ def _run_setup_code(pytxcode_file, globals_):
             end_index = start_index + i
             break
     assert end_index is not None
+    custom_code_lines = lines[start_index:end_index]
+
     # Remove call to setup()
-    custom_code_lines = [line for line in lines[start_index:end_index] if not line.strip().endswith('.setup(pytex)')]
+    # TODO: Handle setting figure/data paths
+    custom_code_lines = [line for line in custom_code_lines
+                         if not line.strip().endswith('.setup(pytex)')]
+
     # Execute
     custom_code = textwrap.dedent(''.join(custom_code_lines))
     exec(compile(custom_code, filename='<pythontexcustomcode>', mode='exec'), globals_)
 
 
 def run_standalone(main):
-    """Turn the calling module into a standalone script which generates its figure."""
+    """Turn the calling module into a standalone script which generates its figure.
+
+    The setup code from the main document will be executed, but the pytex instance will
+    not be available (so `pytex.open` etc will not work).  The figure will be generated
+    as a 4x4" PDF in the current working directory.
+
+    Args:
+        main (Callable[..., str]: The `main` function from a figure script.
+    """
     # TODO: Command-line arguments
+    # TODO: Stub pytex.open etc
     print('Setting up...')
-    _run_setup_code('example.pytxcode', main.__globals__)
+    # TODO: Not hard-coded
+    _run_setup_code('example', main.__globals__)
     _setup_paths()
     _setup_matplotlib()
 
     print('Drawing...')
-    figure_filename = _render_figure(main, width=4, aspect=SQUARE, format_='pdf')
+    figure_filename = _draw_figure(main, width=4, aspect=SQUARE, format_='pdf')
     print('Saved figure as', figure_filename)
